c48303ab7b2a079216fe8151c1085a2d20545667
Eddie Kao <eddie@5xruby.tw>
2019-12-19 14:48:36 +0800

    10 - add flash

--- app/frontend/scripts/shared/application.js
+++ app/frontend/scripts/shared/application.js
@@ -13,3 +13,12 @@ document.addEventListener('turbolinks:load', () => {
   }
 });
 
+document.addEventListener('turbolinks:load', () => {
+  (document.querySelectorAll('.notification .delete') || []).forEach(($delete) => {
+    $notification = $delete.parentNode;
+    $delete.addEventListener('click', () => {
+      $notification.parentNode.removeChild($notification);
+    });
+  });
+});
+
