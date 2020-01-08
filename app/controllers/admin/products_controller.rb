a2ec1640958fc925f90892019f6158d9a5a0ae11
Eddie Kao <eddie@5xruby.tw>
2020-01-08 17:12:43 +0800

    19 - create action for Product

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -5,5 +5,24 @@ class Admin::ProductsController < Admin::BaseController
   def new
     @product = Product.new
   end
+
+  def create
+    @product = Product.new(product_params)
+    
+    if @product.save
+      redirect_to admin_products_path, notice: '商品已新增'
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def product_params
+    params.require(:product).permit(:name, 
+                                    :vendor_id, 
+                                    :list_price, 
+                                    :sell_price)
+  end
 end
 
