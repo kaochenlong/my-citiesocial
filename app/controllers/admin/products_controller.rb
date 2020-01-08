269badf36eb6458ed9c6c25ec05993398eb0419a
Eddie Kao <eddie@5xruby.tw>
2020-01-08 16:24:50 +0800

    18 - add form for Product

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -1,5 +1,9 @@
 class Admin::ProductsController < Admin::BaseController
   def index
   end
+
+  def new
+    @product = Product.new
+  end
 end
 
--- app/models/vendor.rb
+++ app/models/vendor.rb
@@ -1,5 +1,7 @@
 class Vendor < ApplicationRecord
   acts_as_paranoid
   validates :title, presence: true
+
+  scope :available, -> { where(online: true) }
 end
 
