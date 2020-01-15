ad1a8ba03257a0aaa5eb30f0b9410efa540e0d98
Eddie Kao <eddie@5xruby.tw>
2020-01-16 06:38:19 +0800

    27 - add SKU part 4

--- app/frontend/controllers/product_form_controller.js
+++ app/frontend/controllers/product_form_controller.js
@@ -9,5 +9,17 @@ export default class extends Controller {
     let content = this.templateTarget.innerHTML.replace(/NEW_RECORD/g, new Date().getTime());
     this.linkTarget.insertAdjacentHTML('beforebegin', content);
   }
+
+  remove_sku(event) {
+    event.preventDefault();
+    
+    let wrapper = event.target.closest('.nested-fields');
+    if (wrapper.dataset.newRecord == 'true') {
+      wrapper.remove();
+    } else {
+      wrapper.querySelector("input[name*='_destroy']").value = 1;
+      wrapper.style.display = 'none';
+    }
+  }
 }
 
