8b6ec411b1f903212441f69f80b52fd69c67a7f8
Eddie Kao <eddie@5xruby.tw>
2020-02-03 01:01:03 +0800

    32 - Category part 3

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -1,5 +1,8 @@
 class Admin::CategoriesController < Admin::BaseController
+  before_action :find_category, only: [:edit, :update, :destroy]
+
   def index
+    @categories = Category.order(position: :asc)
   end
 
   def new
@@ -16,7 +19,27 @@ class Admin::CategoriesController < Admin::BaseController
     end
   end
 
+  def edit
+  end
+
+  def update
+    if @category.update(category_params)
+      redirect_to admin_categories_path, notice: '分類更新成功'
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @category.destroy
+    redirect_to admin_categories_path, notice: '分類已刪除'
+  end
+
   private
+  def find_category
+    @category = Category.find(params[:id])
+  end
+
   def category_params
     params.require(:category).permit(:name)
   end
