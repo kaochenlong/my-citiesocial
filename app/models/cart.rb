dabd5ad1bafe222171a8052b05a60ec0626a59a4
Eddie Kao <eddie@5xruby.tw>
2020-02-03 12:04:22 +0800

    41 - shopping cart with TDD part 2

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -1,8 +1,15 @@
 class Cart
+  attr_reader :items
+
+  def initialize
+    @items = []
+  end
+
   def add_item(product_id)
+    @items << product_id
   end
 
   def empty?
-    false
+    @items.empty?
   end
 end
--- spec/models/cart_spec.rb
+++ spec/models/cart_spec.rb
@@ -8,6 +8,16 @@ RSpec.describe Cart, type: :model do
       cart.add_item(2)
       expect(cart.empty?).to be false
     end
+
+    it "加了相同種類的商品到購物車裡，購買項目（CartItem）並不會增加，但商品的數量會改變" do
+      cart = Cart.new
+
+      3.times { cart.add_item(1) }
+      2.times { cart.add_item(2) }
+
+      expect(cart.items.count).to be 2
+      expect(cart.items.first.quantity).to be 3
+    end
   end
 
   context "進階功能" do
