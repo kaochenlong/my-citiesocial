4ab852cf78961e55168fa7cd5d541a07ce264973
Eddie Kao <eddie@5xruby.tw>
2020-02-04 11:54:44 +0800

    51 - Using shopping cart part 2

--- app/frontend/controllers/product_controller.js
+++ app/frontend/controllers/product_controller.js
@@ -1,9 +1,33 @@
 import { Controller } from "stimulus" 
+import Rails from "@rails/ujs"
 
 export default class extends Controller { 
-  static targets = [ "quantity" ] 
+  static targets = [ "quantity", "sku", "addToCartButton" ] 
 
-  connect() { 
+  add_to_cart(evt) {
+    evt.preventDefault();
+
+    let product_id = this.data.get("id");
+    let quantity = this.quantityTarget.value;
+    let sku = this.skuTarget.value;
+
+    if (quantity > 0) {
+      this.addToCartButtonTarget.classList.add('is-loading');
+      let data = new FormData();
+      data.append("id", product_id);
+      data.append("quantity", quantity);
+      data.append("sku", sku);
+
+      Rails.ajax({
+        url: "/api/hello", 
+        data, 
+        type: "POST",
+        success: respo => {
+        }, 
+        error: err => {
+        }
+      });
+    }
   }
 
   quantity_minus(evt) {
