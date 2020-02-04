8a59f7c0ca13749ed24afba4dbb2b73e6d39f625
Eddie Kao <eddie@5xruby.tw>
2020-02-04 15:44:23 +0800

    53 - Using shopping cart part 4

--- app/controllers/api/v1/utils_controller.rb
+++ app/controllers/api/v1/utils_controller.rb
@@ -15,11 +15,10 @@ class Api::V1::UtilsController < ApplicationController
     product = Product.friendly.find(params[:id])
 
     if product 
-      cart = Cart.from_hash(session[:cart_9527])
-      cart.add_item(product.code)
-      session[:cart_9527] = cart.serialize
+      current_cart.add_item(product.code)
+      session[:cart_9527] = current_cart.serialize
 
-      render json: {status: 'ok', items: cart.items.count}
+      render json: {status: 'ok', items: current_cart.items.count}
     end
   end
 end
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,7 @@ class ApplicationController < ActionController::Base
   rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
 
   before_action :find_categories, unless: :backend?
+  helper_method :current_cart
 
   private
   def record_not_found
@@ -16,5 +17,9 @@ class ApplicationController < ActionController::Base
 
   def find_categories
     @categories = Category.order(position: :asc)
+  end
+
+  def current_cart
+    @cart9527 ||= Cart.from_hash(session[:cart_9527])
   end
 end
