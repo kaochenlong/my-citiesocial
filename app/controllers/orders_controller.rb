ee2916dc56f29d2f5b26252462f588b0d08e2799
Eddie Kao <eddie@5xruby.tw>
2020-02-07 16:07:36 +0800

    63 - Using Linepay part 4

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -9,9 +9,28 @@ class OrdersController < ApplicationController
     end
 
     if @order.save
-      redirect_to root_path, notice: 'OK'
-    else
-      render 'carts/checkout'
+      resp = Faraday.post("#{ENV['line_pay_endpoint']}/v2/payments/request") do |req|
+        req.headers['Content-Type'] = 'application/json'
+        req.headers['X-LINE-ChannelId'] = ENV['line_pay_channel_id']
+        req.headers['X-LINE-ChannelSecret'] = ENV['line_pay_channel_secret']
+        req.body = {
+          productName: "五百倍大平台", 
+          amount: current_cart.total_price.to_i, 
+          currency: "TWD", 
+          confirmUrl: "http://localhost:3000/orders/confirm", 
+          orderId: @order.num
+        }.to_json
+      end
+
+      result = JSON.parse(resp.body)
+
+      if result["returnCode"] == "0000" 
+        payment_url = result["info"]["paymentUrl"]["web"]
+        redirect_to payment_url
+      else
+        flash[:notice] = '付款發生錯誤'
+        render 'carts/checkout'
+      end
     end
   end
 
